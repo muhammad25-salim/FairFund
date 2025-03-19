389c5a08172adda03b9e1452c63f4d41d78c0aad
muhammad salim <etec02m230441@epu.edu.iq>
2025-03-19 22:39:12 +0300

    Create a MainPage class

--- src/project/Main.java
+++ src/project/Main.java
@@ -1,3 +1,5 @@
+package project;
+
 public class Main {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello to FairFund \n Expense Sharing & Management System");
