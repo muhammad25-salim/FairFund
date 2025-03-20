ceb7c6334920b43eb4c8483e4e0c1a7ec3f5acac
Muhammad_Qadr <mahamadqadr97@gmail.com>
2025-03-20 14:33:04 +0300

    complete MainPage

--- src/project/MainPage.java
+++ src/project/MainPage.java
@@ -1,5 +1,7 @@
 package project;
 
+import org.w3c.dom.Text;
+
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -11,23 +13,21 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 public class MainPage extends Application {
-    private Stage primaryStage; 
-    
+    private Stage primaryStage;
 
     @Override
     public void start(Stage primaryStage) {
-        this.primaryStage = primaryStage; 
+        this.primaryStage = primaryStage;
 
         // Main layout
         BorderPane root = new BorderPane();
         root.setStyle("-fx-background-color: white;");
 
-        
         VBox leftSide = new VBox();
         leftSide.setAlignment(Pos.CENTER);
         leftSide.setStyle("-fx-background-color: #238BFA; -fx-padding: 50px;");
 
-        Image image = new Image("file:assets/illustration.png"); 
+        Image image = new Image("file:assets/illustration.png");
         ImageView imageView = new ImageView(image);
         imageView.setFitWidth(300);
         imageView.setPreserveRatio(true);
@@ -39,8 +39,49 @@ public class MainPage extends Application {
 
         leftSide.getChildren().addAll(imageView, tagline);
 
-        
         VBox rightSide = new VBox(20);
         rightSide.setAlignment(Pos.CENTER);
         rightSide.setPadding(new Insets(50));
-    }}
+
+        Text title = new Text("FAIR FUND");
+        title.setFont(Font.font("Arial", FontWeight.BOLD, 24));
+        title.setFill(javafx.scene.paint.Color.BLACK);
+
+        Text subTitle = new Text("Fair, Fast, Transparent");
+        subTitle.setFont(Font.font("Arial", FontWeight.NORMAL, 14));
+
+        Button createGroupBtn = new Button("Create group");
+        createGroupBtn.setStyle(
+                "-fx-background-color: #238BFA; -fx-text-fill: white; -fx-font-size: 16px; -fx-padding: 10px 20px;");
+        createGroupBtn.setOnAction(e -> openCreateGroup());
+
+        Button joinGroupBtn = new Button("Join an existing group");
+        joinGroupBtn.setStyle(
+                "-fx-background-color: transparent; -fx-border-color: #238BFA; -fx-text-fill: #238BFA; -fx-font-size: 16px; -fx-padding: 10px 20px;");
+        joinGroupBtn.setOnAction(e -> openJoinGroup());
+
+        rightSide.getChildren().addAll(title, subTitle, createGroupBtn, joinGroupBtn);
+
+        root.setLeft(leftSide);
+        root.setCenter(rightSide);
+
+        Scene scene = new Scene(root, 800, 500);
+        primaryStage.setTitle("FairFund - Expense Sharing");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    private void openCreateGroup() {
+        primaryStage.setScene(CreateGroupage.getScene(primaryStage, farFundManager));
+    }
+
+    private void openJoinGroup() {
+        primaryStage.setScene(JoinGroupPage.getScene(primaryStage, farFundManager));
+    }
+
+    public static void launchGUI(FarFundManager farFundManager) {
+        MainPage mainPage = new MainPage(farFundManager);
+        mainPage.start(new Stage());
+
+    }
+}
