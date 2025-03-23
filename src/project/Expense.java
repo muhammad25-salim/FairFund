2af7bc8825b32ad998f2321a36026a8fe00a98cc
Muhammad_Qadr <mahamadqadr97@gmail.com>
2025-03-23 23:31:26 +0300

    creat class Expense

---
+++ src/project/Expense.java
@@ -0,0 +1,54 @@
+package project;
+
+import java.util.List;
+
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleStringProperty;
+
+public class Expense {
+    private SimpleStringProperty title; 
+    private SimpleDoubleProperty totalAmount; 
+    private User payer; 
+    private List<User> participants;
+
+    public Expense(String title, double totalAmount, User payer, List<User> participants) {
+        this.title = new SimpleStringProperty(title);
+        this.totalAmount = new SimpleDoubleProperty(totalAmount);
+        this.payer = payer;
+        this.participants = participants;
+        calculateBalances();
+    }
+
+   
+    public SimpleStringProperty titleProperty() {
+        return title;
+    }
+
+    public SimpleDoubleProperty totalAmountProperty() {
+        return totalAmount;
+    }
+
+    public User getPayer() {
+        return payer;
+    }
+
+    public List<User> getParticipants() {
+        return participants;
+    }
+
+   
+    public String getTitle() {
+        return title.get();
+    }
+
+    public void setTitle(String title) {
+        this.title.set(title);
+    }
+
+    public double getTotalAmount() {
+        return totalAmount.get();
+    }
+
+    public void setTotalAmount(double totalAmount) {
+        this.totalAmount.set(totalAmount);
+    }
