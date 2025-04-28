59420be0d265ab3143f312286dd0ec8130a7f448
AbdullAzeez Shwan <abdulazizshwan@gmail.com>
2025-04-29 00:46:11 +0300

    create UserEntity class

---
+++ src/main/java/database/entities/UserEntity.java
@@ -0,0 +1,50 @@
+package project.database.entities;
+
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.table.DatabaseTable;
+
+@DatabaseTable(tableName = "users")
+public class UserEntity {
+
+    @DatabaseField(generatedId = true) // Auto-generate unique ID
+    private int id;
+
+    @DatabaseField
+    private String name;
+
+    @DatabaseField(foreign = true, foreignAutoRefresh = true, columnName = "group_id")
+    private GroupEntity group;
+
+    public UserEntity() {
+        // ORMLite requires a no-arg constructor
+    }
+
+    public UserEntity(String name, GroupEntity group) {
+        this.name = name;
+        this.group = group;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public GroupEntity getGroup() {
+        return group;
+    }
+
+    public void setGroup(GroupEntity group) {
+        this.group = group;
+    }
+}
