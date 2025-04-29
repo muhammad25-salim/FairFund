d230d21445db9d29718d5cd84aaf8997a9ba3fe4
Muhammad_Qadr <mahamadqadr97@gmail.com>
2025-04-29 18:14:36 +0300

    update main class

--- src/main/java/project/Main.java
+++ src/main/java/project/Main.java
@@ -4,10 +4,33 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 import project.controllers.MainPage;
 import project.models.*;
+import project.database.DatabaseHelper;
+import project.database.entities.GroupEntity;
+import java.sql.SQLException;
+import java.util.List;
 
 public class Main extends Application {
     public static void main(String[] args) {
         launch(args);
+        try {
+            System.setProperty("com.j256.ormlite.logger.level", "ERROR");
+            DatabaseHelper dbHelper = new DatabaseHelper();
+
+            dbHelper.recreateTables();
+
+            // Save a group
+            GroupEntity group = new GroupEntity("nm123","nmuna");
+            dbHelper.saveGroup(group);
+
+            
+            List<GroupEntity> groups = dbHelper.getAllGroups();
+            System.out.println("Groups in database: " + groups.size());
+
+            dbHelper.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
     }
 
     @Override
@@ -17,4 +40,3 @@ public class Main extends Application {
         MainPage mainPage = new MainPage(FairFundManager);
         mainPage.start(primaryStage);
     }
-}
