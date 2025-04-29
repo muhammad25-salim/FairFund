7956df4b424e2b549ce13e4337da4ad9694c72c3
Ahmad Samad <ahmadsamad0781@gmail.com>
2025-04-29 18:33:20 +0300

    update DatabaseHelper

--- src/main/java/project/database/DatabaseHelper.java
+++ src/main/java/project/database/DatabaseHelper.java
@@ -8,7 +8,8 @@ import com.j256.ormlite.table.TableUtils;
 import project.database.entities.GroupEntity;
 import project.database.entities.UserEntity;
 import project.database.entities.ExpenseEntity;
-import java.sql.SQLException; 
+import java.sql.SQLException;
+import java.util.List;
 
 public class DatabaseHelper {
 
@@ -50,4 +51,60 @@ public class DatabaseHelper {
     public void saveUser(UserEntity user) throws SQLException {
         userDao.createOrUpdate(user);
     }
-} 
+
+    public void saveExpense(ExpenseEntity expense) throws SQLException {
+        // This should either create or update the expense in the database
+        Dao.CreateOrUpdateStatus status = expenseDao.createOrUpdate(expense);
+    
+        // Log the result of the createOrUpdate operation
+        if (status.isCreated()) {
+            System.out.println("Expense created in database with ID: " + expense.getId());
+        } else if (status.isUpdated()) {
+            System.out.println("Expense updated in database with ID: " + expense.getId());
+        } else {
+            System.out.println("No changes made to the expense with ID: " + expense.getId());
+        }
+    }
+    
+      
+    public void deleteExpense(ExpenseEntity expense) throws SQLException {
+        // Log for debugging to see which expense is being deleted
+        System.out.println("Deleting expense from database with ID: " + expense.getId());
+        expenseDao.delete(expense);  // Deletes the expense from the database
+    }
+    
+
+    // Retrieve methods
+    public List<GroupEntity> getAllGroups() throws SQLException {
+        return groupDao.queryForAll();
+    }
+
+    public List<UserEntity> getUsersByGroup(GroupEntity group) throws SQLException {
+        return userDao.queryForEq("group_id", group);
+    }
+
+    public List<ExpenseEntity> getExpensesByGroup(String groupId) throws SQLException {
+        return expenseDao.queryForEq("groupId", groupId);
+    }
+
+    public Dao<GroupEntity, String> getGroupDao() {
+        return groupDao;
+    }
+
+    public void close() {
+        if (connectionSource != null) {
+            try {
+                connectionSource.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    // Example of recreating tables
+    public void recreateTables() throws SQLException {
+        TableUtils.createTableIfNotExists(connectionSource, GroupEntity.class);
+        TableUtils.createTableIfNotExists(connectionSource, UserEntity.class);
+        TableUtils.createTableIfNotExists(connectionSource, ExpenseEntity.class);
+    }
+}
