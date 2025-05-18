54bd94f1bf70f13fcbc217fda8345b860ed7ce53
Muhammad_Qadr <mahamadqadr97@gmail.com>
2025-05-19 01:12:57 +0300

    create SignUpPage with modern layout, branding, and responsive design

---
+++ src/main/java/project/controllers/SignUpPage.java
@@ -0,0 +1,52 @@
+package project.controllers;
+
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.scene.shape.Line;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import project.database.DatabaseHelper;
+import project.database.entities.UserEntity;
+import project.utils.ColorManager;
+
+import java.sql.SQLException;
+
+public class SignUpPage {
+
+    public static Scene getScene(Stage primaryStage) {
+        // Main layout container with two sides
+        HBox mainContainer = new HBox();
+        
+        // Left side - Luxury image or gradient panel (40% of width)
+        VBox leftPanel = new VBox();
+        leftPanel.setPrefWidth(480); // 40% of 1200px
+        leftPanel.setStyle(
+            "-fx-background-color: linear-gradient(to bottom right, " + 
+            ColorManager.toRgbString(ColorManager.getPrimaryColor()) + ", " + 
+            "derive(" + ColorManager.toRgbString(ColorManager.getPrimaryColor()) + ", 30%));" +
+            "-fx-effect: dropshadow(three-pass-box, " + ColorManager.toRgbaString(ColorManager.BLACK_SEMI_TRANSPARENT, 0.4) + ", 15, 0, 0, 0);"
+        );
+        
+        // Logo or app icon at the top
+        ImageView logoView = new ImageView();
+        try {
+            Image logoImage = new Image("file:src/main/resources/Image/logo.png");
+            logoView.setImage(logoImage);
+            logoView.setFitHeight(100);
+            logoView.setPreserveRatio(true);
+        } catch (Exception e) {
+            System.out.println("Logo not found, using text instead");
+            Text logoText = new Text("FairFund");
+            logoText.setFont(Font.font("Arial", FontWeight.BOLD, 36));
+            logoText.setFill(ColorManager.TEXT_COLOR);
+            leftPanel.getChildren().add(logoText);
+        }
+    }
+}
