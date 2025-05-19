ae41a1cd793a480a0c4b1fd66d3440fb0eb40e16
muhammad salim <etec02m230441@epu.edu.iq>
2025-05-19 18:56:53 +0300

    add payment class content

--- src/main/java/project/models/Payment.java
+++ src/main/java/project/models/Payment.java
@@ -1,5 +1,60 @@
 package project.models;
 
+import java.time.LocalDate;
+
 public class Payment {
+    private double amount;
+    private String from;
+    private String to;
+    private LocalDate date;
+    private String creator; // Add creator field
+    private int id;         // Add ID field for database reference
+
+    public Payment(double amount, String from, String to, LocalDate date) {
+        this.amount = amount;
+        this.from = from;
+        this.to = to;
+        this.date = date;
+    }
+
+    public Payment(int id, double amount, String from, String to, LocalDate date, String creator) {
+        this.id = id;
+        this.amount = amount;
+        this.from = from;
+        this.to = to;
+        this.date = date;
+        this.creator = creator;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
     
+    public String getCreator() {
+        return creator;
+    }
+
+    public void setCreator(String creator) {
+        this.creator = creator;
+    }
+    
+    public int getId() {
+        return id;
+    }
+    
+    public void setId(int id) {
+        this.id = id;
+    }
 }
