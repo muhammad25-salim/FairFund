cd29910f8e4b6c91ab0551d35b290d730067fbc2
Muhammad_Qadr <mahamadqadr97@gmail.com>
2025-05-19 20:20:04 +0300

    update Database helper

--- src/main/java/project/database/DatabaseHelper.java
+++ src/main/java/project/database/DatabaseHelper.java
@@ -6,23 +6,28 @@ import com.j256.ormlite.jdbc.JdbcConnectionSource;
 import com.j256.ormlite.support.ConnectionSource;
 import com.j256.ormlite.table.TableUtils;
 import project.database.entities.GroupEntity;
+import project.database.entities.MemberEntity;
 import project.database.entities.UserEntity;
 import project.database.entities.ExpenseEntity;
+import project.database.entities.ExpenseParticipantEntity;
+import project.database.entities.PaymentEntity;
+
 import java.sql.SQLException;
+import java.util.ArrayList;
+
 import java.util.List;
 
 public class DatabaseHelper {
 
     private static final String DATABASE_URL = "jdbc:sqlite:fairfund.db";
 
-    static {
-        System.setProperty("com.j256.ormlite.logger.level", "DEBUG");
-    }
-
-    private ConnectionSource connectionSource;
+     private ConnectionSource connectionSource;
     private Dao<GroupEntity, String> groupDao;
-    private Dao<UserEntity, Integer> userDao;
+    private Dao<MemberEntity, Integer> MemberDao;
     private Dao<ExpenseEntity, Integer> expenseDao;
+    private Dao<ExpenseParticipantEntity, Integer> expenseParticipantDao;
+    private Dao<UserEntity, Integer> userDao;
+    private Dao<PaymentEntity, Integer> paymentDao
 
     public DatabaseHelper() throws SQLException {
         connectionSource = new JdbcConnectionSource(DATABASE_URL);
@@ -34,22 +39,43 @@ public class DatabaseHelper {
         TableUtils.createTableIfNotExists(connectionSource, GroupEntity.class);
         TableUtils.createTableIfNotExists(connectionSource, UserEntity.class);
         TableUtils.createTableIfNotExists(connectionSource, ExpenseEntity.class);
+        TableUtils.createTableIfNotExists(connectionSource, ExpenseParticipantEntity.class);
+        TableUtils.createTableIfNotExists(connectionSource, PaymentEntity.class);
+        TableUtils.createTableIfNotExists(connectionSource, MemberEntity.class);
+
+
     }
 
     private void initializeDaos() throws SQLException {
         groupDao = DaoManager.createDao(connectionSource, GroupEntity.class); 
         userDao = DaoManager.createDao(connectionSource, UserEntity.class);
         expenseDao = DaoManager.createDao(connectionSource, ExpenseEntity.class);
+        expenseParticipantDao = DaoManager.createDao(connectionSource, ExpenseParticipantEntity.class);
+        MemberDao = DaoManager.createDao(connectionSource, MemberEntity.class);
+        paymentDao = DaoManager.createDao(connectionSource, PaymentEntity.class);
+
     }
     
 
     // Save methods
+
+       public void saveUser(UserEntity user) throws SQLException {
+        try {
+            // Assuming you have a DAO for UserEntity
+            userDao.create(user);
+            System.out.println("User saved to database: " + user.getUsername());
+        } catch (SQLException e) {
+            System.err.println("Error saving user to database: " + e.getMessage());
+            throw e;
+        }
+    }
+
     public void saveGroup(GroupEntity group) throws SQLException {
         groupDao.createOrUpdate(group); // This should insert or update the group in the database
     }
 
-    public void saveUser(UserEntity user) throws SQLException {
-        userDao.createOrUpdate(user);
+    public void saveMember(MemberEntity Member) throws SQLException {
+        MemberDao.createOrUpdate(Member);
     }
 
     public void saveExpense(ExpenseEntity expense) throws SQLException {
