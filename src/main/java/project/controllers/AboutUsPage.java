9c47267f69a9703ce14ea59eeee8bd692b7f2476
AbdullAzeez Shwan <abdulazizshwan@gmail.com>
2025-05-19 22:16:02 +0300

    adding class AboutUsPage

---
+++ src/main/java/project/controllers/AboutUsPage.java
@@ -0,0 +1,49 @@
+package project.controllers;
+
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
+import javafx.stage.Stage;
+import project.models.FairFundManager;
+import project.utils.ColorManager;
+
+public class AboutUsPage {
+
+    public static Scene getScene(Stage primaryStage, FairFundManager fairFundManager, String groupId) {
+        // Main layout
+        BorderPane mainLayout = new BorderPane();
+        mainLayout.setPadding(new Insets(30));
+
+        // Back Button
+        Button backButton = new Button("« Back");
+        backButton.setStyle(
+                "-fx-background-color: " + ColorManager.toRgbString(ColorManager.getPrimaryColor()) + ";" +
+                "-fx-text-fill: " + ColorManager.toRgbString(ColorManager.TEXT_COLOR) + ";" +
+                "-fx-font-size: 14px;" +
+                "-fx-background-radius: 20;" +
+                "-fx-padding: 6 20 6 20;"
+        );
+        backButton.setOnMouseEntered(e -> backButton.setStyle(
+                "-fx-background-color:rgb(22, 92, 196);" +
+                "-fx-text-fill: " + ColorManager.toRgbString(ColorManager.TEXT_COLOR) + ";" +
+                "-fx-font-size: 14px;" +
+                "-fx-background-radius: 20;" +
+                "-fx-padding: 6 20 6 20;"
+        ));
+        backButton.setOnMouseExited(e -> backButton.setStyle(
+                "-fx-background-color: " + ColorManager.toRgbString(ColorManager.getPrimaryColor()) + ";" +
+                "-fx-text-fill: " + ColorManager.toRgbString(ColorManager.TEXT_COLOR) + ";" +
+                "-fx-font-size: 14px;" +
+                "-fx-background-radius: 20;" +
+                "-fx-padding: 6 20 6 20;"
+        ));
+        backButton.setOnAction(e -> primaryStage.setScene(OverviewPage.getScene(primaryStage, fairFundManager, groupId)));
